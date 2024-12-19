3e40e5c228f5ae122e219d9174238f5747af18c3
NirmalJT <nirmaljyotithakuria@gmail.com>
2024-12-19 19:33:48 +0530

    two practice promblem done

---
+++ Lecture 3/practicePoroblem.js
@@ -0,0 +1,18 @@
+//print all even numbes from 0 to 100
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 == 0) {
+    console.log("num is", i);
+  }
+}
+// Create a game where you start with any random game number. Ask the user to keep
+// guessing the game number until the user enters correct value.
+
+// let num = Math.trunc((Math.random() * 100));
+let gameNumber = "25";
+
+let userNumber = prompt("Enter your number");
+while (userNumber !== gameNumber) {
+  userNumber = prompt("You entered the wrong number Guess again");
+}
+alert("Congratulation you entered the right number");
