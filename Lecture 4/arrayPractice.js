aa634c724324839c84740ef79598cbfa36d5e6ba
NirmalJT <nirmaljyotithakuria@gmail.com>
2024-12-24 10:45:50 +0530

    did two practice problem in array

--- Lecture 4/arrayPractice.js
+++ Lecture 4/arrayPractice.js
@@ -7,17 +7,26 @@ for (let mark of marks) {
   sum += mark;
 }
 let avearage = sum / marks.length;
-console.log(`Average marks of the entire class is = ${avearage}`);
-
+console.log(`The average marks of the entire class is ${avearage}`);
 //For a given array with prices of 5 items ->[250,645,300,900,50] all items have an offer of 10% off on them.change the array to store final price  after  applying offer.
-
-let items = [250, 645, 300, 900, 50];
-
-let i = 0;
-for (let item of items) {
-  let 
-  discountedPrice = item / 10;
-  item = discountedPrice;
-  i++;
+//using for of loop
+let prices = [250, 645, 300, 900, 50];
+let index = 0;
+for (let price of prices) {
+  let discountedPrice = price / 10;
+  console.log(discountedPrice);
+  index++;
+  prices[index - 1] = discountedPrice;
 }
-console.log(items);
+console.log(
+  `The array after applying 10% discounted on the products are ${prices}`
+);
+
+//using for loop
+let itemPrices = [250, 645, 300, 900, 50];
+
+for (let i = 0; i < itemPrices.length; i++) {
+  let Price10D = itemPrices[i] / 10;
+  itemPrices[i] = Price10D;
+}
+console.log(`The array of prices after 10% discount is ${itemPrices}`);
