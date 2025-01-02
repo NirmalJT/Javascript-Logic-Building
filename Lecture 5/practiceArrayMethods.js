d84235cb36c8b4f745cb7ef2c80b0be39ea86e78
NirmalJT <nirmaljyotithakuria@gmail.com>
2025-01-02 12:57:23 +0530

    correct some mistack in the practice problem lecture 5

--- Lecture 5/practiceArrayMethods.js
+++ Lecture 5/practiceArrayMethods.js
@@ -13,9 +13,9 @@ let calSquare = (val) => {
 arr.forEach(calSquare);
 //Qs. We are given array of marks of students. Filter our of the marks of students that scored 90+.
 
-// Use the reduce method to calculate product of all numbers in the array.
 // Qs. Take a number n as input from user. Create an array of numbers from 1 to n.
 // Use the reduce method to calculate sum of all numbers in the array.
+// Use the reduce method to calculate product of all numbers in the array.
 
 let marks = [
   56, 87, 90, 98, 96, 94, 93, 99, 92, 45, 64, 72, 86, 77, 55, 44, 35,
@@ -29,22 +29,6 @@ console.log(
   `The  marks of array that  more 90 or 90 is : [${marks_90_Student}]`
 );
 //Question 2
-let product_Of_Marks = marks.reduce((previous, current) => {
-  return previous * current;
-});
-
-console.log(
-  `The product of all numbers in the  marks  is : [${product_Of_Marks}]`
-);
-
-//Question 4
-
-let sum_of_marks = marks.reduce((previous, current) => {
-  return previous + current;
-});
-console.log(`The sum of all numbers in the  marks  is : [${sum_of_marks}]`);
-
-//Question 3
 
 let n = prompt("Enter a number");
 Array = [];
@@ -52,3 +36,15 @@ for (let i = 1; i <= n; i++) {
   Array[i - 1] = i;
 }
 alert(`Your array is ${Array}`);
+//Question 2 -i)
+
+let sum_of_marks = Array.reduce((previous, current) => {
+  return previous + current;
+});
+alert(`The sum of all numbers in the  array  is : [${sum_of_marks}]`);
+//Question 2 -ii)
+let product_Of_Marks = Array.reduce((previous, current) => {
+  return previous * current;
+});
+
+alert(`The product of all numbers in the  array  is : [${product_Of_Marks}]`);
